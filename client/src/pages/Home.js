856fa18c4f21394ea2212860fefb38ffa708c1e9
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-09-14 22:40:13 -0500

    updated pages_home and partial nav_index

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,37 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
 
 const Home = () => {
   return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
-    </div>
+    <section>
+      {/* <Navbar></Navbar> */}
+      <div className="content">
+        <div className="info">
+          <h2>
+            {" "}
+            Find Your <br />{" "}
+            <span>
+              Furever Friend <i className="fa-solid fa-heart"></i>
+            </span>
+          </h2>
+          <p>
+            This application is an accessible pet social media app where users
+            can search for adoptable pets, friend other pet owners, and comment
+            and like photos.
+          </p>
+          <Link to="/signup" href="./signup" className="signup-btn">
+            sign up now!
+          </Link>
+          <Link to="/login" href="./login" className="login-btn">
+            log in
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
