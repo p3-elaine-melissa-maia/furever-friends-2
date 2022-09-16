6427a7eeeba6911063e3cc25154674599c1ed5b6
ethantor12 <ethantor12@gmail.com>
2022-09-15 22:28:10 -0500

    working on rendering api data

--- client/src/pages/Adopt.js
+++ client/src/pages/Adopt.js
@@ -3,6 +3,8 @@ import React from 'react';
 import '../styles/Home.css'
 
 function Adopt() {
+  const [responses, setResponses] = useState([]);
+  const arr = []
   const [values, setValues] = useState({
     species: "",
     zip: "",
@@ -22,43 +24,77 @@ function Adopt() {
     fetchData();
   };
 
+  useEffect(() => {
+    fetchData()
+  }, [responses])
+
   function fetchData() {
     var petfinder = require("@petfinder/petfinder-js");
     var client = new petfinder.Client({ apiKey: "85M0Le5ywilSAxXmQk5cvNWgrO03XluwDzz2ALbvGHwFQpSk3M", secret: "y6uvIhNy9I6se6Fu9zCRaHY8jf99I2hYSgczYJBg" });
-
     client.animal.search()
       .then(function (response) {
         // Do something with `response.data.animals`
-        console.log(response)
+        // console.log(response.data.animals)
+        arr.push(...response.data.animals)
+        setResponses(...arr)
+        console.log(response.data.animals);
+        console.log(responses)
+        // console.log(data);
+        // setValues(data);
       })
-      
+
       .catch(function (error) {
         // Handle the error
       });
+
   };
 
+
+
   return (
-    <div className="form-container">
-      <form className="pet-finder-form" onSubmit={handleSubmit}>
-        <label>
-          Pick your species:
-          <select value={values.species}
-          onChange={handleSpeciesInput}>
-            <option value="Cat">Cat</option>
-            <option value="Dog">Dog</option>
-          </select>
-        </label>
-        <input
-          type="string"
-          onChange={handleZipInput}
-          value={values.zip}
-          className="form-group"
-          name="zip"
-          placeholder="Zip Code">
-        </input>
-        <button type="submit">Submit</button>
-      </form>
+    <div>
+      {/* !!!-- form to filter searches --! */}
+      <div className="form-container">
+        <form className="pet-finder-form" onSubmit={handleSubmit}>
+          <label>
+            Pick your species:
+            <select value={values.species}
+              onChange={handleSpeciesInput}>
+              <option value="Cat">Cat</option>
+              <option value="Dog">Dog</option>
+            </select>
+          </label>
+          <input
+            type="string"
+            onChange={handleZipInput}
+            value={values.zip}
+            className="form-group"
+            name="zip"
+            placeholder="Zip Code">
+          </input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+
+      <div className="values-container">
+        {console.log(responses)}{
+          responses?.map((value) => {
+            console.log(value)
+            return (
+
+
+              <div className="value-id" key={value.id}>
+                <h2 className="value-name">{value.name}</h2>
+                <h2 className="value-breed">{value.breed}</h2>
+
+              </div>
+            )
+          }
+          )}
+      </div>
     </div>
+
   )
 };
 
