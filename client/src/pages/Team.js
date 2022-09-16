5f96006f62ab70883cc6571faf47d07afc76ba96
Elaine Danahy <edanahy22@gmail.com>
2022-09-16 13:39:04 -0500

    Revert "partially updated team page"

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -3,8 +3,6 @@ import React from "react";
 // import { useQuery } from '@apollo/client';
 import "../styles/Team.css";
 import placeholder from "../assets/placeholder.png";
-import elaine from "../assets/placeholder.jpg";
-import Deven from "../assets/placeholder.png";
 import { Link } from "react-router-dom";
 
 const Team = () => {
@@ -15,15 +13,14 @@ const Team = () => {
           <h2> Our Team </h2>
           <div className="teammember">
             <h3>
-              <img src={elaine} /> <br /> Elaine{" "}
+              <img src={placeholder} /> <br /> Elaine{" "}
             </h3>
           </div>
           <p>
-            Elaine is a school-based speech-language pathologist in the
-            Chicagoland area with a strong interest in software development. She
-            has worked in education for the past four years enhancing student's
-            communication abilities, often using different technology-based
-            systems to aid their skills.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            laboriosam illum ratione quas iste inventore provident laborum,
+            voluptate, reiciendis voluptas officiis distinctio doloribus magni,
+            reprehenderit tempora. Id, molestiae. Ipsam, consequatur.
           </p>
 
           <div className="teammember">
@@ -52,13 +49,14 @@ const Team = () => {
 
           <div className="teammember">
             <h3>
-              <img src={Deven} /> <br /> Melissa{" "}
+              <img src={placeholder} /> <br /> Melissa{" "}
             </h3>
           </div>
           <p>
-            Melissa Deven is currently an Associate Portfolio Manager at the
-            Federal Home Loan Bank of Chicago where she has been for 10 years.
-            She has an interest in full stack web development.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            laboriosam illum ratione quas iste inventore provident laborum,
+            voluptate, reiciendis voluptas officiis distinctio doloribus magni,
+            reprehenderit tempora. Id, molestiae. Ipsam, consequatur.
           </p>
         </div>
       </div>
