e66e5a5e1308dba26e00c058d07da8fedf6bdbdf
Elaine Danahy <edanahy22@gmail.com>
2022-09-15 22:27:37 -0500

    edits for heroku

--- client/src/pages/Adopt.js
+++ client/src/pages/Adopt.js
@@ -63,30 +63,31 @@ function Adopt() {
   )
 };
 
-function Adopt() {
-  const url = "https://api.petfinder.com/v2/animals"
-  const [pets, setPets] = useState(null)
+// function Adopt() {
+//   const url = "https://api.petfinder.com/v2/animals"
+//   const [pets, setPets] = useState(null)
 
-  useEffect(() => {
-    fetch(url)
-      .then(response => {
-        return response.json();
+//   useEffect(() => {
+//     fetch(url)
+//       .then(response => {
+//         return response.json();
     
-      })
-      .then((data) => {
-      setPets(data)
-      })
-  }, []);
+//       })
+//       .then((data) => {
+//       setPets(data)
+//       })
+//   }, []);
 
-  if (pets) {
-    return <>
-      <PetFinderForm/><div></div>
-    </>
-  };
+//   if (pets) {
+//     return <>
+//       <PetFinderForm/><div></div>
+//     </>
+//   };
 
-    return <>
-      <PetFinderForm/><div></div>
-    </>
+//     return <>
+//       <PetFinderForm/><div></div>
+//     </>
   
-};
+// };
 
+export default Adopt;
